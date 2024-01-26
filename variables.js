b4d0c1f3f98231337f0e522e83dd6b4f7f5d3fe7
NandlalShah955 <shahnandlal49@gmail.com>
2024-01-26 20:07:42 +0530

    Commit done

--- variables.js
+++ variables.js
@@ -1,3 +1,34 @@
-let a = 1;
+"use strict"; // treat all Js code as new Version
+// We can declare varaible without declaring them also
+let a;
+let b = null;
 
-console.log(a, "first");
+console.log(a, b, "result");
+
+// Prefer not to use var instead try to use let and var
+
+// Conversion
+// Trying to convert string into number
+// "123dfk"=NaN
+
+let isLogged = 2;
+let isfaltu = 0;
+
+let faltu = Boolean(isfaltu);
+let isBoolean = Boolean(isLogged);
+console.log(isBoolean); //true
+console.log(faltu); //false
+// "htitesh" => true
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
