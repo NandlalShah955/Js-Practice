086c9b7d8c6703f345a6e47222d17802633ff583
NandlalShah955 <shahnandlal49@gmail.com>
2024-01-27 17:44:28 +0530

    async await

--- Part-2/Promise.js
+++ Part-2/Promise.js
@@ -1,17 +1,31 @@
 console.log("first");
 // A basic promise example
+// let naam = "namit";
 const promise = new Promise((resolve, reject) => {
   let num = 4;
   if (num == 5) {
-    resolve();
+    resolve({ username: "amit", password: "12345" });
   } else {
-    reject();
+    reject("Error");
   }
 });
 promise
-  .then(() => {
-    console.log("Success");
+  .then((naam) => {
+    // console.log("Success", naam);
+    return naam.username;
   })
   .catch(() => {
     console.log("error");
   });
+
+//   Or
+
+async function consumePromise() {
+  try {
+    let res = await promise;
+    console.log(res, "res");
+  } catch (error) {
+    console.log(error);
+  }
+}
+consumePromise();
