bfc392fa98bd9a3d3ecb1ecb1117bfb1ebd33f9a
NandlalShah955 <shahnandlal49@gmail.com>
2024-01-27 14:28:32 +0530

    Map function

---
+++ HOF.js
@@ -0,0 +1,17 @@
+console.log("first");
+// forEach example
+// For each doesn't return anything
+let LaxmiPG = ["mukul", "Vishalsir", "nandlal", "sandeep"];
+let finalperson = LaxmiPG.forEach((el) => {
+  console.log(el, "elin loops ");
+  return el;
+});
+console.log("finalperson", finalperson);
+// Map return the value
+
+let allperson = LaxmiPG.map((el) => {
+  console.log(el, "el in loops");
+  return el;
+});
+
+console.log("allperson", allperson);
