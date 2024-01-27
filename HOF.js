30ce5c5e8273e92073d38062da00bf75c7e4d2da
NandlalShah955 <shahnandlal49@gmail.com>
2024-01-27 14:38:04 +0530

    Done

--- HOF.js
+++ HOF.js
@@ -18,3 +18,9 @@ console.log("allperson", allperson);
 
 // Reduce method
 let arr = [2, 4, 6, 6, 76, 23];
+let finalarr = arr.reduce(function (acc, currentval) {
+  console.log(`my accumalator is ${acc} and my currentval${currentval}`);
+  return acc + currentval;
+}, 0);
+
+console.log("finalarr", finalarr);
